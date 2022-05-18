11d90d22e34d1735e380cb24fdc9a75495895599
saraivais <isadorasaraiva95@gmail.com>
2022-05-18 17:20:57 -0300

    feat: creates new actions & thunk thingy~ thunk thingy used to request api data & then dispatch to modify store w another action~

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,22 @@
 // Coloque aqui suas actions
 export const SAVE_EMAIL = 'SAVE_EMAIL';
+export const REQUEST_CURRENCIES = 'REQUEST_CURRENCIES';
+export const GET_CURRENCIES = 'GET_CURRENCIES';
 
 export const saveEmail = (emailAdress) => ({ type: SAVE_EMAIL, payload: emailAdress });
+
+export const requestCurrencies = () => ({ type: REQUEST_CURRENCIES });
+
+export const getCurrencies = (currencyArray) => ({
+  type: GET_CURRENCIES, payload: currencyArray });
+
+export function getCurrencyFromAPI() {
+  return async (dispatch) => {
+    dispatch(requestCurrencies());
+    const apiReturn = await fetch('https://economia.awesomeapi.com.br/json/all');
+    const apiData = await apiReturn.json();
+    const allCurrArray = Object.keys(apiData);
+    const filteredCurrencies = allCurrArray.filter((currencies) => currencies !== 'USDT');
+    dispatch(getCurrencies(filteredCurrencies));
+  };
+}
