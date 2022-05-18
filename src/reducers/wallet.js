6072e82df48c5900486dcf4571a05fe6c302a5ed
saraivais <isadorasaraiva95@gmail.com>
2022-05-18 17:21:33 -0300

    feat: creates cases in switch for two new actions~

--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,4 +1,6 @@
 // Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+import { REQUEST_CURRENCIES, GET_CURRENCIES } from '../actions';
+
 const INITIAL_STATE = {
   currencies: [],
   expenses: [],
@@ -6,6 +8,10 @@ const INITIAL_STATE = {
 
 function wallet(state = INITIAL_STATE, action) {
   switch (action.type) {
+  case GET_CURRENCIES:
+    return { ...state, currencies: action.payload };
+  case REQUEST_CURRENCIES:
+    return state;
   default:
     return state;
   }
