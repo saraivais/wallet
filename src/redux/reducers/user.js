f7e9b75a182bf9b74e01a374562544672eba9540
saraivais <isadorasaraiva95@gmail.com>
2022-05-18 15:25:44 -0300

    feat: creates basic user reducer & initial state~

---
+++ src/redux/reducers/user.js
@@ -0,0 +1,13 @@
+// Esse reducer será responsável por tratar as informações da pessoa usuária
+const INITIAL_STATE = {
+  email: '',
+};
+
+function user(state = INITIAL_STATE, action) {
+  switch (action.type) {
+  default:
+    return state;
+  }
+}
+
+export default user;
