8dcbf5a0b72a63dfef0e33d599ef1bedcadd73af
saraivais <isadorasaraiva95@gmail.com>
2022-05-18 16:26:37 -0300

    fix: change import for store~ removes redux folder

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import { Provider } from 'react-redux';
 import App from './App';
-import store from './redux/store';
+import store from './store';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
