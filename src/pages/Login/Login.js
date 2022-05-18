b988461416938c3e9ebcf09abccb6ac94361209f
saraivais <isadorasaraiva95@gmail.com>
2022-05-18 15:50:49 -0300

    feat: creates input & controlls them w component state~

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,8 +1,49 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      loginMail: '',
+      loginPassword: '',
+    };
+  }
+
+  handleInputChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
   render() {
-    return <div>Login~</div>;
+    const { loginMail, loginPassword } = this.state;
+    return (
+      <>
+        <div>Login!</div>
+        <label htmlFor="loginMail">
+          E-mail:
+          <input
+            type="email"
+            name="loginMail"
+            data-testid="email-input"
+            value={ loginMail }
+            onChange={ this.handleInputChange }
+          />
+        </label>
+        <label htmlFor="loginPassword">
+          Senha:
+          <input
+            type="password"
+            name="loginPassword"
+            data-testid="password-input"
+            value={ loginPassword }
+            onChange={ this.handleInputChange }
+          />
+        </label>
+
+      </>
+    );
   }
 }
 
