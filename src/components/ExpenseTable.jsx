a45d36377831fdab07cf9ca0313ebfaa6e0c7b52
saraivais <isadorasaraiva95@gmail.com>
2022-05-18 20:32:20 -0300

    feat: expenseTable component linked to redux~ receives expense array as props, then uses map to dinamically create table rows for each expense~

--- src/components/ExpenseTable.jsx
+++ src/components/ExpenseTable.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
+import propTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 class ExpenseTable extends React.Component {
   render() {
+    const { allExpenses } = this.props;
+    // console.log(allExpenses);
     return (
       <table>
         <tbody>
@@ -16,10 +20,33 @@ class ExpenseTable extends React.Component {
             <th>Moeda de conversão</th>
             <th>Editar/Excluir</th>
           </tr>
+          {allExpenses
+            .map(({ currency, description, id, method, tag, value, exchangeRates }) => (
+              <tr key={ id }>
+                <td>{ description }</td>
+                <td>{ tag }</td>
+                <td>{ method }</td>
+                <td>{ Number(value).toFixed(2) }</td>
+                <td>{ exchangeRates[currency].name.split('/')[0]}</td>
+                <td>{ Number(exchangeRates[currency].ask).toFixed(2) }</td>
+                <td>{ (value * exchangeRates[currency].ask).toFixed(2) }</td>
+                <td>Real</td>
+                <td>
+                  <button type="button" data-testid="delete-btn">Excluir</button>
+                </td>
+              </tr>))}
         </tbody>
       </table>
     );
   }
 }
 
-export default ExpenseTable;
+const mapStateToProps = (state) => ({
+  allExpenses: state.wallet.expenses,
+});
+
+ExpenseTable.propTypes = {
+  allExpenses: propTypes.arrayOf(propTypes.shape()).isRequired,
+};
+
+export default connect(mapStateToProps)(ExpenseTable);
