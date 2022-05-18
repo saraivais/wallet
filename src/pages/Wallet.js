c6094e20966aeaff9111bcb83e7300face7e7dc2
saraivais <isadorasaraiva95@gmail.com>
2022-05-18 20:05:13 -0300

    feat: imports and renders ExpenseTable in Wallet~

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -4,6 +4,7 @@ import propTypes from 'prop-types';
 import { getCurrencyFromAPI } from '../actions';
 import Header from '../components/Header';
 import ExpenseForm from '../components/ExpenseForm';
+import ExpenseTable from '../components/ExpenseTable';
 
 class Wallet extends React.Component {
   // using componentDidMount to check the api return~
@@ -23,6 +24,7 @@ class Wallet extends React.Component {
       <>
         <Header />
         <ExpenseForm />
+        <ExpenseTable />
         <div>TrybeWallet</div>
       </>
     );
