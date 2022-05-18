6c13f8d1890dfb1190467f93a29adf5dc13f3323
saraivais <isadorasaraiva95@gmail.com>
2022-05-18 19:44:53 -0300

    feat: controlls inputs~ also receives nextId from state, dispatches thunk thingy as prop saveThisExpense~

--- src/components/ExpenseForm.jsx
+++ src/components/ExpenseForm.jsx
@@ -1,26 +1,80 @@
 import React from 'react';
 import propTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { getCurrencyListFromAPI } from '../actions';
 
 class ExpenseForm extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      valueSpent: '',
+      expenseDescription: '',
+      currencyUsed: 'USD',
+      paymentMethod: 'Dinheiro',
+      expenseTag: 'Alimentação',
+    };
+  }
+
+  handleInputChange = ({ target }) => {
+    const { id, value } = target;
+    this.setState({
+      [id]: value,
+    });
+  }
+
+  resetForms = () => {
+    this.setState({
+      valueSpent: '',
+      expenseDescription: '',
+      currencyUsed: 'USD',
+      paymentMethod: 'Dinheiro',
+      expenseTag: 'Alimentação',
+    });
+  }
+
   render() {
-    const { currencyList } = this.props;
+    const { currencyList, nextId, saveThisExpense } = this.props;
+    const {
+      valueSpent,
+      expenseDescription,
+      currencyUsed,
+      paymentMethod,
+      expenseTag,
+    } = this.state;
+
     return (
       <form>
 
         <label htmlFor="valueSpent">
           Valor:
-          <input id="valueSpent" type="number" data-testid="value-input" />
+          <input
+            id="valueSpent"
+            type="number"
+            data-testid="value-input"
+            value={ valueSpent }
+            onChange={ this.handleInputChange }
+          />
         </label>
 
         <label htmlFor="expenseDescription">
           Descrição:
-          <input id="expenseDescription" type="text" data-testid="description-input" />
+          <input
+            id="expenseDescription"
+            type="text"
+            data-testid="description-input"
+            value={ expenseDescription }
+            onChange={ this.handleInputChange }
+          />
         </label>
 
-        <label htmlFor="currency">
+        <label htmlFor="currencyUsed">
           Moeda
-          <select id="currency">
+          <select
+            id="currencyUsed"
+            value={ currencyUsed }
+            onChange={ this.handleInputChange }
+          >
             {currencyList
               .map((currency, index) => (
                 <option
@@ -34,7 +88,12 @@ class ExpenseForm extends React.Component {
         </label>
 
         <label htmlFor="paymentMethod">
-          <select id="paymentMethod" data-testid="method-input">
+          <select
+            id="paymentMethod"
+            data-testid="method-input"
+            value={ paymentMethod }
+            onChange={ this.handleInputChange }
+          >
             <option value="Dinheiro">Dinheiro</option>
             <option value="Cartão de crédito">Cartão de crédito</option>
             <option value="Cartão de débito">Cartão de débito</option>
@@ -42,7 +101,12 @@ class ExpenseForm extends React.Component {
         </label>
 
         <label htmlFor="expenseTag">
-          <select id="expenseTag" data-testid="tag-input">
+          <select
+            id="expenseTag"
+            data-testid="tag-input"
+            value={ expenseTag }
+            onChange={ this.handleInputChange }
+          >
             <option value="Alimentação">Alimentação</option>
             <option value="Lazer">Lazer</option>
             <option value="Trabalho">Trabalho</option>
@@ -50,6 +114,22 @@ class ExpenseForm extends React.Component {
             <option value="Saúde">Saúde</option>
           </select>
         </label>
+
+        <button
+          type="button"
+          onClick={ () => {
+            saveThisExpense({
+              value: valueSpent,
+              description: expenseDescription,
+              currency: currencyUsed,
+              method: paymentMethod,
+              tag: expenseTag,
+            }, nextId);
+            this.resetForms();
+          } }
+        >
+          Adicionar despesa
+        </button>
       </form>
     );
   }
@@ -57,11 +137,20 @@ class ExpenseForm extends React.Component {
 
 const mapStateToProps = (state) => ({
   currencyList: state.wallet.currencies,
+  nextId: state.wallet.expenses.length,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  saveThisExpense: (formObject, id) => dispatch(getCurrencyListFromAPI(formObject, id)),
 });
 
 ExpenseForm.propTypes = {
   currencyList: propTypes.arrayOf(propTypes.string).isRequired,
+  nextId: propTypes.number.isRequired,
+  saveThisExpense: propTypes.func.isRequired,
 };
 
 // export default ExpenseForm;
-export default connect(mapStateToProps)(ExpenseForm);
+export default connect(mapStateToProps, mapDispatchToProps)(ExpenseForm);
+
+// Atenção nesse ponto: você deverá fazer uma requisição para API e buscar a cotação no momento que o botão de `Adicionar despesa` for apertado. Para isso você deve utilizar um thunk.
