18c4b1f5ab2841775b0c3e5c9a2d1706afd07899
saraivais <isadorasaraiva95@gmail.com>
2022-05-18 17:23:38 -0300

    feat: creates dispatchToProps & calls func~ thunk thingy called as prop in component did mount to update store & prop validation~

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import propTypes from 'prop-types';
+import { getCurrencyFromAPI } from '../actions';
 import Header from '../components/Header';
 
 class Wallet extends React.Component {
+  // using componentDidMount to check the api return~
+  // async componentDidMount() {
+  //   const apiresult = await fetch('https://economia.awesomeapi.com.br/json/all');
+  //   const apiData = await apiresult.json();
+  //   console.log(apiData);
+  // }
+
+  componentDidMount() {
+    const { saveCurrenciesToGlobalState } = this.props;
+    saveCurrenciesToGlobalState();
+  }
+
   render() {
     return (
       <>
@@ -12,4 +27,14 @@ class Wallet extends React.Component {
   }
 }
 
-export default Wallet;
+const mapDispatchToProps = (dispatch) => ({
+  saveCurrenciesToGlobalState: () => dispatch(getCurrencyFromAPI()),
+});
+
+Wallet.propTypes = {
+  saveCurrenciesToGlobalState: propTypes.func.isRequired,
+};
+
+export default connect(null, mapDispatchToProps)(Wallet);
+
+// export default Wallet;
