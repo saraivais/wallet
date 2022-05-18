046279a67aad95cfbcb705147e9eb0eb561959d6
saraivais <isadorasaraiva95@gmail.com>
2022-05-18 19:40:18 -0300

    feat: creates 2 more cases for wallet reducers~ dependant on actions save_expenses & calculate_expenses~

--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,9 +1,16 @@
 // Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
-import { REQUEST_CURRENCIES, GET_CURRENCIES } from '../actions';
+import {
+  REQUEST_CURRENCIES,
+  GET_CURRENCIES,
+  SAVE_EXPENSES,
+  CALCULATE_EXPENSES,
+} from '../actions';
+import expenseCalculator from '../helpers/ExpenseCalculator';
 
 const INITIAL_STATE = {
   currencies: [],
   expenses: [],
+  total: 0,
 };
 
 function wallet(state = INITIAL_STATE, action) {
@@ -12,6 +19,10 @@ function wallet(state = INITIAL_STATE, action) {
     return { ...state, currencies: action.payload };
   case REQUEST_CURRENCIES:
     return state;
+  case SAVE_EXPENSES:
+    return { ...state, expenses: [...state.expenses, action.payload] };
+  case CALCULATE_EXPENSES:
+    return { ...state, total: expenseCalculator(state.expenses) };
   default:
     return state;
   }
