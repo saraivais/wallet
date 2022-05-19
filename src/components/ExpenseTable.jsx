a807af81c41482edff10d7725f90343086f8103a
saraivais <isadorasaraiva95@gmail.com>
2022-05-18 22:03:04 -0300

    feat: adds delete function to one specific expense~ uses mapDispatch to props to receive delete function~

--- src/components/ExpenseTable.jsx
+++ src/components/ExpenseTable.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import propTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { removeExpenseAndRecalculateTotal } from '../actions';
 
 class ExpenseTable extends React.Component {
   render() {
-    const { allExpenses } = this.props;
+    const { allExpenses, deleteThisExpense } = this.props;
     // console.log(allExpenses);
     return (
       <table>
@@ -32,7 +33,14 @@ class ExpenseTable extends React.Component {
                 <td>{ (value * exchangeRates[currency].ask).toFixed(2) }</td>
                 <td>Real</td>
                 <td>
-                  <button type="button" data-testid="delete-btn">Excluir</button>
+                  <button
+                    type="button"
+                    data-testid="delete-btn"
+                    onClick={ () => deleteThisExpense(id) }
+                  >
+                    Excluir
+
+                  </button>
                 </td>
               </tr>))}
         </tbody>
@@ -45,8 +53,14 @@ const mapStateToProps = (state) => ({
   allExpenses: state.wallet.expenses,
 });
 
+const mapDispatchToProps = (dispatch) => ({
+  deleteThisExpense:
+  (idToDelete) => dispatch(removeExpenseAndRecalculateTotal(idToDelete)),
+});
+
 ExpenseTable.propTypes = {
   allExpenses: propTypes.arrayOf(propTypes.shape()).isRequired,
+  deleteThisExpense: propTypes.func.isRequired,
 };
 
-export default connect(mapStateToProps)(ExpenseTable);
+export default connect(mapStateToProps, mapDispatchToProps)(ExpenseTable);
